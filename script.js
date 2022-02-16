eae75e593384bf784e5e4c019d37da799887db1c
Jeremy T <jeremytupas@gmail.com>
2022-02-15 22:08:33 -0600

    Initial layout

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hi');
+let userLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  
+}
+
+function makeCard() {
+
+}
