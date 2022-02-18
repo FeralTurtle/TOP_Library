423c38c3ca76be2737fe219fd16333349f843160
Jeremy T <jeremytupas@gmail.com>
2022-02-17 19:16:30 -0600

    Gets user input and stores data as an object in array. Results to console.

--- script.js
+++ script.js
@@ -8,9 +8,30 @@ function Book(author, title, pages, read) {
 }
 
 function addBookToLibrary() {
-  
+  const newBook = new Book(author.value, title.value, pages.value, readBool());
+  userLibrary.push(newBook);
+  console.log(userLibrary);
 }
 
-function makeCard() {
-
+function readBool() {
+  if (readYes.checked) {
+    return 'read';
+  } else if (readNo.checked) {
+    return 'notRead';
+  };
 }
+
+
+
+
+const author = document.querySelector('#author');
+const title = document.querySelector('#title');
+const pages = document.querySelector('#pages');
+
+const readYes = document.querySelector('#read-yes');
+const readNo = document.querySelector('#read-no');
+
+const addBookBtn = document.querySelector('.button-add-book');
+const formSubmitBtn = document.querySelector('#form-btn');
+
+formSubmitBtn.addEventListener('click', () => console.log(addBookToLibrary()));
